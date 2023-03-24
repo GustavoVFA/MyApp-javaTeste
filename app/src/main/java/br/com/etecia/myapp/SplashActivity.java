ff758278bfc7ac573fd61b3892dac63d3919ab45
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-23 22:09:04 -0300

    Botões(  - Configurando ação do botão  - Configurando Toast )

--- app/src/main/java/br/com/etecia/myapp/SplashActivity.java
+++ app/src/main/java/br/com/etecia/myapp/SplashActivity.java
@@ -2,9 +2,11 @@ package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
     Button btnAbrirLogin;
@@ -20,7 +22,8 @@ public class SplashActivity extends AppCompatActivity {
         btnAbrirLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                Toast.makeText(getApplicationContext(), "Cliquei", Toast.LENGTH_SHORT).show();
             }
         });
     }
